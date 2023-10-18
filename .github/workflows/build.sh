c5d04b63a77fadcadf5927cbe3d80ec551dd9371
Giles Knap <giles.knap@diamond.ac.uk>
2023-10-18 07:32:30 +0000

    adopt new template

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,94 +1,47 @@
 #!/bin/bash
 
-# A script for building EPICS container images.
+# A generic build script for epics-containers ioc repositories
 #
 # Note that this is implemented in bash to make it portable between
 # CI frameworks. This approach uses the minimum of GitHub Actions.
-# Also works locally for testing outside of CI (with podman-docker installed)
+# Also works locally for testing outside of CI
 #
 # INPUTS:
 #   PUSH: if true, push the container image to the registry
 #   TAG: the tag to use for the container image
-#   REGISTRY: the container registry to push to
-#   REPOSITORY: the container repository to push to
+#   PLATFORM: the platform to build for (linux/amd64 or linux/arm64)
 #   CACHE: the directory to use for caching
 
-ARCH=${ARCH:-linux}
-PUSH=${PUSH:-false}
-TAG=${TAG:-latest}
-REGISTRY=${REGISTRY:-ghcr.io}
-if [[ -z ${REPOSITORY} ]] ; then
-    # For local builds, infer the registry from git remote (assumes ghcr)
-    REPOSITORY=$(git remote -v | sed  "s/.*@github.com:\(.*\)\.git.*/ghcr.io\/\1/" | tail -1)
-    echo "inferred registry ${REPOSITORY}"
-fi
+export EC_TAG="--tag ${TAG:-latest}"
+export EC_PLATFORM="--platform ${PLATFORM:-linux/amd64}"
+export EC_CACHE="${CACHE:-/tmp/ec-cache}"
+export EC_DEBUG=true
+if [[ "${PUSH}" == 'true' ]] ; then EC_PUSH='--push' ; fi
 
-NEWCACHE=${CACHE}-new
+THIS=$(dirname ${0})
+set -xe
+mkdir -p ${CACHE}
 
-if ! docker -v 2> /dev/null; then
-    echo "switching to podman ..."
-    PODMAN=true
-    shopt -s expand_aliases
-    alias docker=podman
+# get the current version of ec CLI
+pip install -r ${THIS}/../../requirements.txt
 
-    # podman command line parameters (just use local cache)
-    cachefrom=""
-    cacheto=""
-else
-    # setup a buildx driver for multi-arch / remote cached builds
-    docker buildx create --driver docker-container --use
-    # docker command line parameters
-    cachefrom=--cache-from=type=local,src=${CACHE}
-    cacheto=--cache-to=type=local,dest=${NEWCACHE},mode=max
-fi
+# add cache arguments - local file cache passed by github seems to be most reliable
+export EC_CARGS="
+    --cache-from type=local,src=${EC_CACHE}
+    --cache-to type=local,dest=${EC_CACHE}
+"
 
-set -e
+# add extra cross compilation platforms below if needed  e.g.
+#   ec dev build  --arch rtems ... for RTEMS cross compile
 
-do_build() {
-    ARCHITECTURE=$1
-    TARGET=$2
-    shift 2
+# build runtime and developer images
+ec dev build --buildx ${EC_TAG} ${EC_PLATFORM} ${EC_PUSH} ${EC_CARGS}
 
-    image_name=${REGISTRY}/${REPOSITORY}-${ARCHITECTURE}-${TARGET}:${TAG}
-    # convert to lowercase - required for OCI URLs
-    image_name=${image_name,,}
-    args="
-        --build-arg TARGET_ARCHITECTURE=${ARCHITECTURE}
-        --target ${TARGET}
-        -t ${image_name}
-    "
+# extract the ioc schema from the runtime image
+ec dev launch-local ${EC_TAG} --execute \
+'ibek ioc generate-schema /epics/links/ibek/*.ibek.support.yaml' > ibek.ioc.schema.json
 
-    if [[ ${PUSH} == "true" && ${PODMAN} != "true" ]] ; then
-        args="--push "${args}
-    fi
+# run acceptance tests
+shopt -s nullglob # expand to nothing if no tests are found
+for t in "${THIS}/../../tests/*.sh"; do ${t}; done
 
-    echo "CONTAINER BUILD FOR ${image_name} with ARCHITECTURE=${ARCHITECTURE} ..."
-
-    (
-        set -x
-        docker buildx build ${args} ${*} .
-    )
-
-    if [[ ${PUSH} == "true" && ${PODMAN} == "true" ]] ; then
-        podman push ${image_name}
-    fi
-}
-
-# EDIT BELOW FOR YOUR BUILD MATRIX REQUIREMENTS
-#
-# All builds should use cachefrom and the last should use cacheto
-# The last build should execute all stages for the cache to be fully useful.
-#
-# intermediate builds should use cachefrom but will also see the local cache
-#
-# If None of the builds use all stages in the Dockerfile then consider adding
-# cache-to to more than one build. But note there is a tradeoff in performance
-# as every layer will get uploaded to the cache even if it just came out of the
-# cache.
-
-do_build ${ARCH} developer ${cachefrom}
-do_build ${ARCH} runtime ${cachefrom} ${cacheto}
-
-# remove old cache to avoid indefinite growth
-rm -rf ${CACHE}
-mv ${NEWCACHE} ${CACHE}
