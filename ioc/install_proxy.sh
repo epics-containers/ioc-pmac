fcb9d35347fb0dfb437d1b90d31999338a5a1f1a
Giles Knap <giles.knap@diamond.ac.uk>
2024-03-21 11:56:46 +0000

    update rtems-proxy 0.2.3

--- ioc/install_proxy.sh
+++ ioc/install_proxy.sh
@@ -17,7 +17,7 @@
 # so proxy installation is not required.
 
 if [[ $EPICS_TARGET_ARCH == "RTEMS-beatnik" ]] ; then
-    pip install rtems-proxy=="0.2.2"
+    pip install rtems-proxy=="0.2.3"
 fi
 
 # no other proxies supported at present
