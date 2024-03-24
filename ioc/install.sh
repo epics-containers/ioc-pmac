f67a361b66cd9758d30d321fb787a457c4fab38b
Giles Knap <giles.knap@diamond.ac.uk>
2024-03-24 12:43:02 +0000

    fix ioc install.sh

--- ioc/install.sh
+++ ioc/install.sh
@@ -4,7 +4,9 @@
 # every install.sh script in ibek-support should call this script
 # before it compiles its support module.
 
-x86_cfg='./configure/CONFIG_SITE.Common.linux-x86_64'
+THIS=$(realpath $(dirname $0))
+
+x86_cfg="${THIS}/configure/CONFIG_SITE.Common.linux-x86_64"
 
 # for RTEMS builds don't build for the host architecture, target only
 if [[ $EPICS_TARGET_ARCH == "RTEMS"* ]]; then
